cbc6ed077c0e7fce5487763f9ec49fda62b0333d
xox55264 <xox55264@gmail.com.tw>
2021-03-16 20:46:16 +0800

    change route

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
-@app.route('/')
+@app.route('/chatbot')
 def hello_world():
     return 'Hello, World!'
 
