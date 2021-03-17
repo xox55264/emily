ca650e80029ffa294d23ef9d64b2979136103bc1
xox55264 <xox55264@gmail.com.tw>
2021-03-17 20:15:49 +0800

    update accounting module

--- utils/helper.py
+++ utils/helper.py
@@ -38,6 +38,9 @@ class StatusHelper(object):
     def set_status(self, status, user_id):
         return self.redis_helper.set_value(f'{user_id}_status', status)
 
+    def reset_status(self, user_id):
+        return self.redis_helper.delete_key(f'{user_id}_status')
+
     def check_status(self, status, user_id):
         return self.redis_helper.get_value(f'{user_id}_status') == self.config[status]['prev_status']
 
