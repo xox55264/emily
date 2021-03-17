cca9c26b21b70bf1482c6682d510999ce5785a35
xox55264 <xox55264@gmail.com.tw>
2021-03-17 13:29:07 +0800

    update accounting module

--- intentions/accounting.py
+++ intentions/accounting.py
@@ -1,8 +1,6 @@
 from intentions import intention
 
 class Accounting(intention):
-    def __init__(self):
-        super(Accounting, self).__init__()
 
     def start_accounting(self, data):
         pass
